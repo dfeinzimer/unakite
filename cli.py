605edfea5ec9b82eac9b4aa14de97a158429c5a7
David Feinzimer <dfeinzimer@gmail.com>
2018-07-23 23:48:20 -0700

    1.0.1

--- cli.py
+++ cli.py
@@ -1,16 +1,54 @@
 from socket import *
 import commands
+import os
+import sys
 
-serverName = 'localhost'
-serverPort = 12000
+#Check for correct usage
+if len(sys.argv) < 3:
+	print "USAGE python " + sys.argv[0] + " <SERVER MACHINE>" + " <PORT NUMBER>" 
+else:
+	serverName = sys.argv[1]	
+	serverPort = int(sys.argv[2])
+
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
-data = "Hello world"
 
-bytesSent = 0
+def send_control(payload):
+	data = payload
+	bytesSent = 0
+	while bytesSent != len(data):
+			bytesSent += clientSocket.send(data[bytesSent:])
+	print "Sent " + payload
+	return
 
-while bytesSent != len(data):
-	bytesSent += clientSocket.send(data[bytesSent:])
+while 1:
+	try:
+		arg = raw_input("ftp> ")
+	except SyntaxError:
+		arg = None
+	if (arg == 'ls'):
+		print "Yall want it"		
+		send_control(arg)
+		continue
+	elif (arg == 'quit'):
+		print "Disconnecting"
+		break
+	else:
+		print "Unrecognized command"
+		continue
+
+
+#----------------
+
+#data = "Hello world"
+#bytesSent = 0
+#while bytesSent != len(data):
+	#bytesSent += clientSocket.send(data[bytesSent:])
+	#print "Sent data"
+
+#----------------
+
+
 
 clientSocket.close()
--- serv.py
+++ serv.py
@@ -1,12 +1,16 @@
 from socket import *
 import commands
+import os
+import sys
 
-serverPort = 12000
+# Command line checks 
+if len(sys.argv) < 2:
+	print "USAGE python " + sys.argv[0] + " <PORT NUMBER>" 
+else:
+	serverPort = int(sys.argv[1])
 
 #Create socket for control
 SOCKET_control = socket(AF_INET, SOCK_STREAM)
-
-#Bind to the lowest available port
 SOCKET_control.bind(('',serverPort))
 
 #Listen for inbound requests
@@ -15,6 +19,11 @@ SOCKET_control.listen(1)
 print "The server is ready to receive"
 print "SOCKET_control running on port", SOCKET_control.getsockname()[1]
 
+def execute_control(data):
+	for line in commands.getstatusoutput(data):
+		print line
+	return
+	
 while 1:
 	connection,addr = SOCKET_control.accept()
 	tmpBUFF = ""
@@ -24,8 +33,11 @@ while 1:
 		if not tmpBUFF:
 			break
 		data += tmpBUFF
-	print data
+		print data
+		if (data == 'ls'):
+			execute_control(data)
+			data = ""
+			break
 	connection.close()	
 
-#for line in commands.getstatusoutput('ls -l'):
-#	print line
+
