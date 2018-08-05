e57dc47acc0ca9b34b2d489b252d66d14d9d397e
David Feinzimer <dfeinzimer@gmail.com>
2018-08-05 14:10:52 -0700

    1.0.2

--- cli.py
+++ cli.py
@@ -5,50 +5,73 @@ import sys
 
 #Check for correct usage
 if len(sys.argv) < 3:
-	print "USAGE python " + sys.argv[0] + " <SERVER MACHINE>" + " <PORT NUMBER>" 
+	print "USAGE python " + sys.argv[0] + " <SERVER MACHINE>" + " <PORT NUMBER>"
 else:
-	serverName = sys.argv[1]	
+	serverName = sys.argv[1]
 	serverPort = int(sys.argv[2])
 
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
+SOCKET_control = socket(AF_INET, SOCK_STREAM)
+SOCKET_control.connect((serverName, serverPort))
 
+def build_header(data):
+	dataSizeStr = str(len(data) + 1)
+	while len(dataSizeStr) < 10:
+			dataSizeStr = "0" + dataSizeStr
+	data = dataSizeStr + " " + data
+	return data
 
-def send_control(payload):
-	data = payload
-	bytesSent = 0
-	while bytesSent != len(data):
-			bytesSent += clientSocket.send(data[bytesSent:])
-	print "Sent " + payload
+def send(payload):
+	numSent = 0
+	while len(payload) > numSent:
+			numSent += SOCKET_control.send(payload[numSent:])
 	return
 
-while 1:
+def recvAll(sock, numBytes):
+	recvBuff = ""
+	tmpBuff = ""
+	while len(recvBuff) < numBytes:
+		tmpBuff =  sock.recv(numBytes)
+		if not tmpBuff:
+			break
+		recvBuff += tmpBuff
+	print "recvAll recevied:", len(recvBuff),
+	return recvBuff
+
+def getresponse(Response_Socket):
+	Response_Socket.listen(1)
+	connection,addr = Response_Socket.accept()
+	fileData = ""
+	recvBuff = ""
+	fileSize = 0
+	fileSizeBuff = ""
+	fileSizeBuff = recvAll(connection, 10)
+	if len(fileSizeBuff) != 0:
+		print "fileSizeBuff", fileSizeBuff
+		fileSize = int(fileSizeBuff)
+		print "The file size is:", fileSize
+		fileData = recvAll(connection, fileSize)
+		print "The file data is:"
+		print fileData
+	return
+
+while True:
 	try:
-		arg = raw_input("ftp> ")
+		input = raw_input("ftp> ")
 	except SyntaxError:
-		arg = None
-	if (arg == 'ls'):
-		print "Yall want it"		
-		send_control(arg)
-		continue
-	elif (arg == 'quit'):
+		input = None
+	args = input.split()
+	if ((args[0] == 'ls') | (args[0] == 'get') | (args[0] == 'put')):
+		SOCKET_data = socket(AF_INET, SOCK_STREAM)
+		SOCKET_data.bind(('',0))
+		input += " " + serverName + " " + str(SOCKET_data.getsockname()[1])
+		input = build_header(input)
+		send(input)
+		getresponse(SOCKET_data)
+	elif (args[0] == 'quit'):
 		print "Disconnecting"
 		break
 	else:
 		print "Unrecognized command"
 		continue
 
-
-#----------------
-
-#data = "Hello world"
-#bytesSent = 0
-#while bytesSent != len(data):
-	#bytesSent += clientSocket.send(data[bytesSent:])
-	#print "Sent data"
-
-#----------------
-
-
-
-clientSocket.close()
+SOCKET_control.close()
